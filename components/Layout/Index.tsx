acee939401190d8be4b498f06bffb8ffbfb3da65
Olaleye Blessing <Olayinkablexxy@gmail.com>
2021-12-23 22:43:51 +0100

    remove global navbar - each page(for the moment has specific navbar)

--- components/Layout/Index.tsx
+++ components/Layout/Index.tsx
@@ -4,7 +4,7 @@ import { Navbar } from "../../components";
 const Index: FC = ({ children }) => {
     return (
         <>
-            <Navbar />
+            {/* <Navbar /> */}
             {children}
         </>
     );
