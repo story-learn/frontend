fa04796a552e9835b00cc59154cf35180d18b0f0
Olaleye Blessing <Olayinkablexxy@gmail.com>
2022-02-07 16:02:02 +0100

    add temporary notFound page

---
+++ pages/404.tsx
@@ -0,0 +1,11 @@
+import type { NextPage } from "next";
+
+const ErrorPage: NextPage = () => {
+    return (
+        <>
+            <main>Not Found Page</main>
+        </>
+    );
+};
+
+export default ErrorPage;
